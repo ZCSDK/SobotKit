5557aaa12e74a3ab63d44fb33c07b95429b7252b
lizh <lizh@sobot.com>
2023-11-03 09:03:29 +0800

    326

--- SobotKit.framework/Headers/SobotLog.h
+++ SobotKit.framework/Headers/SobotLog.h
@@ -69,6 +69,8 @@ typedef NS_ENUM(NSInteger,SobotLogType) {
  @param logType 日志类型
  */
 +(void)cacheLog:(NSString *) title content:(NSString *)message logType:(SobotLogType) logType;
++(void)addMessageToCache:(NSString *) message type:(SobotLogType )logType;
+
 /**
  *  根据设定的过期时长，清理日志，默认是1天
  */
